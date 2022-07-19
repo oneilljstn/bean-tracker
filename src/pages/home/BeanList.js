1914cf7e4aaff39496c1473191982188438e10b8
Justin O'Neill <oneilljstn@gmail.com>
2022-07-19 15:45:09 +1000

    Added functionality to filter beans by title. Search is done locally - no firebase querying.

--- src/pages/home/BeanList.js
+++ src/pages/home/BeanList.js
@@ -1,32 +1,51 @@
-import React from 'react'
-import styles from './Home.module.css'
-import placeholder from '../../assets/969696.png'
-import { useFirestore } from '../../hooks/useFirestore'
+import React from "react";
+import styles from "./Home.module.css";
+import placeholder from "../../assets/969696.png";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function BeanList({ beans }) {
-  const { deleteDocument } = useFirestore("beans")
+export default function BeanList({ beans, query }) {
+  const { deleteDocument } = useFirestore("beans");
 
- const handleDelete = (beanId) => {
-  deleteDocument(beanId)
- }
+  const handleDelete = (beanId) => {
+    deleteDocument(beanId);
+  };
+
+  const filteredBeans = beans.filter((bean) => {
+    // if the query string is empty show all beans
+    if (query === ""){
+      return true
+    }
+    //otherwise only show beans with matching titles
+    if (query !== '') {
+      return bean.name.toLowerCase().match(query.toLowerCase());
+    } 
+  });
 
   return (
     <div>
-      
       <div className={styles.beanList}>
-        {beans.map((bean) => (
+        {filteredBeans.map((bean) => (
           <div key={bean.id} className={styles.beanCard}>
-            <img className={styles.beanCardImg} src={bean.image ? bean.image : placeholder} alt={bean.name}></img>
-            
-              <p className={styles.beanCardTitle}> {bean.name}</p>
-              <div className={styles.beanDetails}>
+            <img
+              className={styles.beanCardImg}
+              src={bean.image ? bean.image : placeholder}
+              alt={bean.name}
+            ></img>
+
+            <p className={styles.beanCardTitle}> {bean.name}</p>
+            <div className={styles.beanDetails}>
               {/**<p className={styles.beanCardSubTitle}>&#9881;</p>*/}
-              <p className={styles.beanCardContent}>&#9201; {bean.grinderDuration} sec</p>
-              
-              <p className={styles.beanCardContent}> @ setting {bean.grinderSetting}</p>
-              </div>
-              <p className={styles.beanCardNotes}>{bean.notes} </p>
-            
+              <p className={styles.beanCardContent}>
+                &#9201; {bean.grinderDuration} sec
+              </p>
+
+              <p className={styles.beanCardContent}>
+                {" "}
+                @ setting {bean.grinderSetting}
+              </p>
+            </div>
+            <p className={styles.beanCardNotes}>{bean.notes} </p>
+
             <button onClick={() => handleDelete(bean.id)}>DELETE</button>
           </div>
         ))}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,7 @@ import styles from './Home.module.css'
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useCollection } from '../../hooks/useCollection'
 
-import React from 'react'
+import React, { useState } from 'react'
 import AddBeanForm from './AddBeanForm'
 import BeanList from './BeanList'
 
@@ -11,28 +11,44 @@ import BeanList from './BeanList'
 
 export default function Home() {
 
+  const [query, setQuery] = useState('')
   const { user } = useAuthContext()
-  const { documents, error } = useCollection(
-    "beans",
-    ["userId", "==", user.uid],
-    ["createdAt", "desc"]
-  )
- 
- 
+
+    const { documents, error } = useCollection(
+      "beans",
+      ["userId", "==", user.uid],
+      ["createdAt", "desc"]
+    )
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    console.log(query)
+    
+  }
 
 
   return (
+    <>
+    <form onSubmit={handleSearch} className={styles.search}>
+        <input 
+        type="search" 
+        onChange={(e) => setQuery(e.target.value)}
+        value={ query }
+        placeholder="Search"
+        required/>
+    </form>
     <div className={styles.container}>
       
       {<AddBeanForm  uid={user.uid}/>}
     
       <div className={styles.content}>
           {error && <p>{error}</p>}
-          {documents && <BeanList beans={documents} />}
+          {documents && <BeanList beans={documents} query={query}/>}
       </div>
       
 
     
     </div>
+    </>
   )
 }
